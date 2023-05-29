3cd5a4acb7452d071ac24deefd9c04988bb81d47
ghazimoradi <ghazimoradi.amin@gmail.com>
2023-05-30 00:16:33 +0330

    finishing CryptoAdapter

---
+++ app/src/main/java/com/example/concurrencyapp/adapter/CryptoAdapter.kt
@@ -0,0 +1,98 @@
+package com.example.concurrencyapp.adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.concurrencyapp.R
+import com.example.concurrencyapp.databinding.ItemBinding
+import com.example.concurrencyapp.response.ResponseCoinsList
+import com.example.concurrencyapp.utils.Constants
+import com.example.concurrencyapp.utils.roundToThreeDecimals
+import com.example.concurrencyapp.utils.toDoubleFloatPairs
+import javax.inject.Inject
+
+class CryptosAdapter @Inject() constructor() : RecyclerView.Adapter<CryptosAdapter.ViewHolder>() {
+
+    private lateinit var binding: ItemBinding
+    private lateinit var context: Context
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        binding = ItemBinding.inflate(inflater, parent, false)
+        context = parent.context
+        return ViewHolder()
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(differ.currentList[position])
+        holder.setIsRecyclable(false)
+    }
+
+    inner class ViewHolder() : RecyclerView.ViewHolder(binding.root) {
+
+        @SuppressLint("SetTextI18n")
+        fun bind(item: ResponseCoinsList.ResponseCoinsListItem) {
+            binding.apply {
+                tvName.text = item.id
+                tvSymbol.text = item.symbol?.uppercase()
+                tvPrice.text = "€${item.currentPrice?.roundToThreeDecimals()}"
+                imgCrypto.load(item.image) {
+                    crossfade(true)
+                    crossfade(500)
+                    placeholder(R.drawable.round_currency_bitcoin_24)
+                    error(R.drawable.round_currency_bitcoin_24)
+                }
+
+                lineChart.gradientFillColors =
+                    intArrayOf(
+                        Color.parseColor("#2a9085"),
+                        Color.TRANSPARENT
+                    )
+
+                lineChart.animation.duration = Constants.animationDuration
+                val listData = item.sparklineIn7d?.price.toDoubleFloatPairs()
+                lineChart.animate(listData)
+
+                root.setOnClickListener {
+                    onItemClickListener?.let {
+                        it(item)
+                    }
+                }
+            }
+        }
+    }
+
+    private var onItemClickListener: ((ResponseCoinsList.ResponseCoinsListItem) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (ResponseCoinsList.ResponseCoinsListItem) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    private val differCallback =
+        object : DiffUtil.ItemCallback<ResponseCoinsList.ResponseCoinsListItem>() {
+            override fun areItemsTheSame(
+                oldItem: ResponseCoinsList.ResponseCoinsListItem,
+                newItem: ResponseCoinsList.ResponseCoinsListItem
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: ResponseCoinsList.ResponseCoinsListItem,
+                newItem: ResponseCoinsList.ResponseCoinsListItem
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+
+    val differ = AsyncListDiffer(this, differCallback)
+}
---
+++ app/src/main/java/com/example/concurrencyapp/utils/Extension.kt
@@ -0,0 +1,45 @@
+package com.example.concurrencyapp.utils
+
+import android.content.Context
+import android.view.View
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import java.text.DecimalFormat
+
+fun View.isVisible(isShowLoading: Boolean, container: View) {
+    if (isShowLoading) {
+        this.visibility = View.VISIBLE
+        container.visibility = View.GONE
+    } else {
+        this.visibility = View.GONE
+        container.visibility = View.VISIBLE
+    }
+}
+
+fun Context.getCompatColor(@ColorRes colorId: Int) =
+    ResourcesCompat.getColor(resources, colorId, null)
+
+fun Context.getCompatDrawable(@DrawableRes drawableId: Int) =
+    AppCompatResources.getDrawable(this, drawableId)!!
+
+fun RecyclerView.initRecycler(layoutManager: RecyclerView.LayoutManager, adapter: RecyclerView.Adapter<*>) {
+    this.adapter=adapter
+    this.layoutManager=layoutManager
+}
+
+fun List<Double?>?.toDoubleFloatPairs(): List<Pair<String, Float>> {
+    return this!!.map { d ->
+        val f = d!!.toFloat()
+        val s = d.toString()
+        Pair(s, f)
+    }
+}
+
+private val formatter2= DecimalFormat("##.##")
+private val formatter3= DecimalFormat("##.###")
+
+fun Double.roundToTwoDecimals() = formatter2.format(this).toString()
+fun Double.roundToThreeDecimals() = formatter3.format(this).toString()
