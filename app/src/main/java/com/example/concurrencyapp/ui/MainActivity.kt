9aa26184e5b9feb9a758b18b5b828ab7b465db6f
ghazimoradi <ghazimoradi.amin@gmail.com>
2023-05-30 11:11:43 +0330

    fix bug

--- app/src/main/java/com/example/concurrencyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/concurrencyapp/ui/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.concurrencyapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.concurrencyapp.R
 import com.example.concurrencyapp.databinding.ActivityMainBinding
